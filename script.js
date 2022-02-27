a54dc863fb2a2d715d7d32baff24f4b9c548cad6
Aotrek@2022ota <aotrekccu@gmail.com>
2022-02-27 13:26:33 +0600

    asdf

--- script.js
+++ script.js
@@ -15,5 +15,5 @@ function read(text){
         seach.text='please try again'
     }
     document.getElementById('res').innerText=seach.text
-    window.SpeechSynthesis.speak(seach);
+    window.speechSynthesis.speak(seach);
 }

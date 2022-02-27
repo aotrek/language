51d3ed7ebb38a1e0d42dd2cbe01d3f1b02352d13
Aotrek@2022ota <aotrekccu@gmail.com>
2022-02-27 13:08:33 +0600

    this is a first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let myFun = new webkitSpeechRecognition();
+myFun.onresult = (e)=>{
+    let text = e.results[0][0].transcript;
+    document.getElementById('text').innerText=text;
+    read(text)
+}
+function read(text){
+    let seach = new webkitSpeechSynthesisUtterance();
+    seach.text = text;
+    if (text='hello') {
+        seach.text ='bye'
+    }else if(text=='what is your name'){
+        seach.text='my name is swapon'
+    }else{
+        seach.text='please try again'
+    }
+    document.getElementById('res').innerText=seach.text
+    window.SpeechSynthesis.speck(seach);
+}
